c1f0a312a3fb8f933e93adf4b82d0954231b4dbb
daniel wang <daniel867181@gmail.com>
2021-06-28 22:44:50 +1000

    a little there

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import json
 import requests
 from textblob import TextBlob
+import networkx as nx
 
 def create_url():
-    # (supermarket OR woolies OR woolworths) lang:en -is:retweet
+    # (supermarket OR food OR grocery OR retail) lang:en 
+    query = "(supermarket%20OR%20food%20OR%20grocery%20OR%20retail)%20lang%3Aen%20"
     tweet_fields = "tweet.fields=public_metrics,author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query=(supermarket%20OR%20woolies%20OR%20woolworths)%20lang%3Aen%20-is%3Aretweet&{}".format(tweet_fields)
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(query, tweet_fields)
     return url
 
 
@@ -25,17 +27,39 @@ def connect_to_endpoint(url, headers):
         )
     return response.json()
 
+def get_author_ids(dump):
+    data = dump['data']
+    author_id = []
+    for tweet in data:
+        author_id.append(tweet['author_id'])
+    return author_id
 
-def main():
+# def get_friends_list(user_id):
+
+def connectivity(network):
+    nw_graph = {user.id:user.follows for user in network}
+    G = nx.graph(nw_graph)
+    nx.centre(G)
+    pr = nx.pagerank(G)
+    nodes = sorted([(node, pagerank) for node, pagerank in pr.items()], key=lambda x:pr[x[0]])
+    
+
     bearer_token = "AAAAAAAAAAAAAAAAAAAAALP7QwEAAAAAILlOlUyBZKqKiOmhwghZe4xQTzw%3DeAcDSNrGucfsV9LPyU01Y2cxObLspZfeJoCQ94pzld9MaEIUXI"
     url = create_url()
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    dump = json.loads(json.dumps(json_response, indent=4, sort_keys=True))
+    print(dump)
+    author_id = get_author_ids(dump)
+    print(author_id)
+
 
 
 if __name__ == "__main__":
     main()
+
+
+
 # import tweepy
 # from textblob import TextBlob
 
